18b2c8ad3cfa74429b2d2d60e5aacd70a2e31657
Ziggers <123231732+Ziggers@users.noreply.github.com>
2023-09-23 21:56:45 +0800

    Add files via upload

---
+++ EmployeeProject/src/employee/version2/CommissionEmployee.java
@@ -0,0 +1,36 @@
+package employee.version2;
+
+public class CommissionEmployee extends Employee {
+    private double totalSales;
+
+    public CommissionEmployee() {
+        super();
+    }
+
+    public CommissionEmployee(int empID, String empName, MyDate empDateHired, MyDate empBirthDate, double totalSales) {
+        super(empID, empName, empDateHired, empBirthDate);
+        this.totalSales = totalSales;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    @Override
+    public double computeSalary() {
+  
+        if (totalSales < 50000) {
+            return totalSales * 0.05;
+        } else if (totalSales < 100000) {
+            return totalSales * 0.20;
+        } else if (totalSales < 500000) {
+            return totalSales * 0.30;
+        } else {
+            return totalSales * 0.50;
+        }
+    }
+}
---
+++ EmployeeProject/src/employee/version3/PieceWorkerEmployee.java
@@ -0,0 +1,38 @@
+package employee.version3;
+
+public class PieceWorkerEmployee extends Employee {
+    private float totalPiecesFinished;
+    private float ratePerPiece;
+
+    public PieceWorkerEmployee() {
+        super();
+    }
+
+    public PieceWorkerEmployee(int empID, String firstName, String lastName, MyDate empDateHired, MyDate empBirthDate,
+                               float totalPiecesFinished, float ratePerPiece) {
+        super(empID, firstName, lastName, empDateHired, empBirthDate);
+        this.totalPiecesFinished = totalPiecesFinished;
+        this.ratePerPiece = ratePerPiece;
+    }
+
+    public float getTotalPiecesFinished() {
+        return totalPiecesFinished;
+    }
+
+    public void setTotalPiecesFinished(float totalPiecesFinished) {
+        this.totalPiecesFinished = totalPiecesFinished;
+    }
+
+    public float getRatePerPiece() {
+        return ratePerPiece;
+    }
+
+    public void setRatePerPiece(float ratePerPiece) {
+        this.ratePerPiece = ratePerPiece;
+    }
+
+    @Override
+    public double computeSalary() {
+        return totalPiecesFinished * ratePerPiece + (totalPiecesFinished / 100) * ratePerPiece * 10;
+    }
+}
